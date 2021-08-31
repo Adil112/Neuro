d6e0141c008dadd9533698bbe6089f4b5ef28e14
Adil112 <adil.200191@gmail.com>
2021-08-31 19:37:32 +0700

    push

---
+++ Neuro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Neuro
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            var dataset = new List<Tuple<double, double[]>>
+            {
+                new Tuple<double, double[]> (0.01, new double[] { 0.1, 0.1, 0.1}),
+                new Tuple<double, double[]> (0.006, new double[] { 0.1, 0.2, 0.3}),
+                new Tuple<double, double[]> (0.175, new double[] { 0.5, 0.7, 0.5}),
+                new Tuple<double, double[]> (0.012, new double[] { 0.6, 0.1, 0.2}),
+                new Tuple<double, double[]> (0.009, new double[] { 0.1, 0.3, 0.3}),
+                new Tuple<double, double[]> (0.004, new double[] { 0.1, 0.4, 0.1}),
+                new Tuple<double, double[]> (0.0255, new double[] { 0.51, 0.5, 0.1}),
+                new Tuple<double, double[]> (0.0246, new double[] { 0.41, 0.6, 0.1}),
+                new Tuple<double, double[]> (0.0518, new double[] { 0.1, 0.7, 0.74}),
+                new Tuple<double, double[]> (0.3172, new double[] { 0.61, 0.8, 0.65}),
+                new Tuple<double, double[]> (0.0639, new double[] { 0.71, 0.9, 0.1}),
+                new Tuple<double, double[]> (0.0405, new double[] { 0.1, 0.9, 0.45}),
+                new Tuple<double, double[]> (0.0054, new double[] { 0.1, 0.1, 0.54}),
+                new Tuple<double, double[]> (0.02904, new double[] { 0.8, 0.11, 0.33}),
+                new Tuple<double, double[]> (0.0108, new double[] { 0.1, 0.12, 0.9})
+            };
+            
+
+            var topology = new Topology(3, 1, 0.3, 2, 2);
+            var neuralNetwork = new NeuralNetworks(topology);
+            var difference = neuralNetwork.Learn(dataset, 500); // обучаем
+
+            var results = new List<double>();
+            foreach (var data in dataset)
+            {
+                results.Add(neuralNetwork.FeedForward(data.Item2).Output); // используем
+            }
+            double res = 0;
+            for (int i = 0; i < results.Count; i++)
+            {
+                var expected = Math.Round(dataset[i].Item1, 4);
+                var actual = Math.Round(results[i], 4);
+                double promres = (Math.Pow(expected - actual, 2) / expected) * 100;
+                res += promres;
+            }
+            Console.WriteLine($"Общая погрешность: {res / results.Count}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NeuroTests/NeuralNetworksTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neuro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neuro.Tests
+{
+    [TestClass()]
+    public class NeuralNetworksTests
+    {
+        [TestMethod()]
+        public void FeedForwardTest()
+        {
+            var dataset = new List<Tuple<double, double[]>>
+            {
+                /*  2 входных нейрона месяц и день
+                 *  1 скрытый слой c 2 нейронами
+                 *  1 выходной
+                 *  месяц задается как (номер месяца по порядку/12)
+                 *  день задается как (номер дня по порядку/29-31) в зависмости от месяца
+                 *  знак зодиака соответсвенно (знако зодиака по порядку/12)
+                 */
+                //                                                M       D
+                // водолей
+                new Tuple<double, double[]> (1/12, new double[] { 1/12, 20/31}),
+                new Tuple<double, double[]> (1/12, new double[] { 2/12, 3/28}),
+                new Tuple<double, double[]> (1/12, new double[] { 2/12, 19/28}),
+                //рыбы
+                new Tuple<double, double[]> (2/12, new double[] { 2/12, 20/28}),
+                new Tuple<double, double[]> (2/12, new double[] { 3/12, 5/31}),
+                new Tuple<double, double[]> (2/12, new double[] { 3/12, 20/31}),
+                //овен
+                new Tuple<double, double[]> (3/12, new double[] { 3/12, 21/31}),
+                new Tuple<double, double[]> (3/12, new double[] { 4/12, 5/30}),
+                new Tuple<double, double[]> (3/12, new double[] { 4/12, 19/30}),
+                //телец
+                new Tuple<double, double[]> (4/12, new double[] { 4/12, 20/30}),
+                new Tuple<double, double[]> (4/12, new double[] { 5/12, 5/31}),
+                new Tuple<double, double[]> (4/12, new double[] { 5/12, 20/31}),
+                //близнецы
+                new Tuple<double, double[]> (5/12, new double[] { 5/12, 21/31}),
+                new Tuple<double, double[]> (5/12, new double[] { 6/12, 5/30}),
+                new Tuple<double, double[]> (5/12, new double[] { 6/12, 20/30}),
+                //рак
+                new Tuple<double, double[]> (6/12, new double[] { 6/12, 21/30}),
+                new Tuple<double, double[]> (6/12, new double[] { 7/12, 6/31}),
+                new Tuple<double, double[]> (6/12, new double[] { 7/12, 22/31}),
+                //лев
+                new Tuple<double, double[]> (7/12, new double[] { 7/12, 23/31}),
+                new Tuple<double, double[]> (7/12, new double[] { 8/12, 7/31}),
+                new Tuple<double, double[]> (7/12, new double[] { 8/12, 22/31}),
+                //дева
+                new Tuple<double, double[]> (8/12, new double[] { 8/12, 23/31}),
+                new Tuple<double, double[]> (8/12, new double[] { 9/12, 8/30}),
+                new Tuple<double, double[]> (8/12, new double[] { 9/12, 22/30}),
+                //весы
+                new Tuple<double, double[]> (9/12, new double[] { 9/12, 23/30}),
+                new Tuple<double, double[]> (9/12, new double[] { 10/12, 8/31}),
+                new Tuple<double, double[]> (9/12, new double[] { 10/12, 23/31}),
+                //скорпион  
+                 new Tuple<double, double[]> (10/12, new double[] { 10/12, 24/31}),
+                new Tuple<double, double[]> (10/12, new double[] { 11/12, 8/30}),
+                new Tuple<double, double[]> (10/12, new double[] { 11/12, 22/30}),
+                //стрелец
+                new Tuple<double, double[]> (11/12, new double[] { 11/12, 23/30}),
+                new Tuple<double, double[]> (11/12, new double[] { 12/12, 8/31}),
+                new Tuple<double, double[]> (11/12, new double[] { 12/12, 21/31}),
+                //козерог
+                new Tuple<double, double[]> (12/12, new double[] { 12/12, 21/31}),
+                new Tuple<double, double[]> (12/12, new double[] { 1/12, 7/31}),
+                new Tuple<double, double[]> (12/12, new double[] { 1/12, 19/31})
+            };
+            var topology = new Topology(2, 1, 0.1, 2, 2);
+            var neuralNetwork = new NeuralNetworks(topology);
+            var difference = neuralNetwork.Learn(dataset, 1000); // обучаем
+
+            var results = new List<double>();
+            foreach(var data in dataset)
+            {
+                results.Add(neuralNetwork.FeedForward(data.Item2).Output); // используем
+            }
+
+            for(int i =0; i < results.Count; i++)
+            {
+                var expected = Math.Round(dataset[i].Item1, 4);
+                var actual = Math.Round(results[i], 4);
+                //Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
